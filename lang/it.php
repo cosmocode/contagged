e877cb216ed5ddc9925ff8c283b9decc421a5a4c
Simone Piccardi <piccardi@truelite.it>
2006-05-18 08:58:52 +0200

    Italian translation
    darcs-hash:20060518065852-bfcc8-7d8b1098521d2791cfcbfdb4c9a14f1b53048608.gz

---
+++ lang/it.php
@@ -0,0 +1,84 @@
+<?
+
+$lang['ldapab']           = 'The LDAP address book';
+
+$lang['name']             = 'Cognome';
+$lang['givenname']        = 'Nome';
+$lang['title']            = 'Titolo';
+$lang['organization']     = 'Azienda';
+$lang['office']           = 'Ufficio';
+$lang['street']           = 'Via';
+$lang['zip']              = 'Codice Postale';
+$lang['location']         = 'Citt';
+$lang['phone']            = 'Telefono';
+$lang['fax']              = 'FAX';
+$lang['mobile']           = 'Cellulare';
+$lang['pager']            = 'Pager';
+$lang['homephone']        = 'Telefono (priv.)';
+$lang['homestreet']       = 'Indirizzo (priv.)';
+$lang['photo']            = 'Foto';
+$lang['url']              = 'Sito Web';
+$lang['note']             = 'Note';
+$lang['mail']             = 'E-Mail';
+$lang['manager']          = 'Key Account';
+
+$lang['anniversary']      = 'Compleanno';
+$lang['marker']           = 'Etichetta';
+$lang['tagcloud']         = 'Tutte le etichette in ordine alfaberico. Le piu usate sono piu grandi.';
+
+$lang['business']         = 'Affari';
+$lang['private']          = 'Privato';
+$lang['extended']         = 'Informazione addizionale';
+
+$lang['delphoto']         = 'Cancella foto esistente';
+
+$lang['notloggedin']      = 'Non connesso';
+$lang['loggedinas']       = 'Connesso come';
+$lang['login']            = 'Connetti';
+$lang['logout']           = 'Disconnetti';
+$lang['edit']             = 'Modifica';
+$lang['show']             = 'Mostra';
+$lang['new']              = 'Nuovo';
+$lang['delete']           = 'Cancella';
+$lang['copy']             = 'Copia';
+$lang['csvexport']        = 'CSV';
+$lang['vcfexport']        = 'VCard';
+$lang['vcfimport']        = 'Importa';
+$lang['search']           = 'Cerca';
+$lang['submit']           = 'Invia';
+$lang['help']             = 'Aiuto';
+$lang['select']           = 'Seleziona';
+$lang['orgs']             = 'Aziende';
+$lang['upload']           = 'Carica';
+
+$lang['publicbook']       = 'Indirizzario pubblico';
+$lang['privatebook']      = 'Indirizzario privato';
+
+$lang['username']         = 'Username';
+$lang['password']         = 'Password';
+$lang['remember']         = 'Rimani connesso su questo computer';
+
+$lang['msg_login']        = 'Collegarsi, prego!';
+$lang['msg_loginfail']    = 'Accessi fallito!';
+$lang['msg_reallydel']    = 'Cancella veramente?';
+$lang['msg_addto']        = 'Scegli l\'indirizzario a cui aggiungere la voce:';
+$lang['msg_jpegonly']     = 'Solo JPEG';
+$lang['msg_dateformat']   = 'YYYY-MM-DD';
+$lang['msg_uploadvcf']    = 'Upload VCard File';
+$lang['msg_tagsep']       = 'Separato da virgole';
+
+$lang['err_noentries']    = 'Nessuna voce';
+$lang['err_ldap']         = 'Il server LDAP ha restituito i seguenti errori';
+
+$lang['openxchange']      = 'Xchange Information';
+$lang['moreopenxchange']  = 'Xchange Information Cont\'d';
+$lang['instantmessenger'] = 'Inst Msg';
+$lang['categories']       = 'Categorie';
+$lang['birthday']         = 'Compleanno';
+$lang['domain']           = 'Dominio e-mail';
+$lang['country']          = 'Paese';
+$lang['certificate']      = 'Certificato x.509';
+$lang['timezone']         = 'Fuso orario';
+$lang['position']         = 'Posizione';
+$lang['ipphone']          = 'Telefono IP';
+?>
