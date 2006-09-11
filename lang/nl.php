930dc1a6957af1546e141147050d076121ff254d
Riny Heijdendael <riny@heijdendael.nl>
2006-09-11 12:28:56 +0200

    Dutch language file
    darcs-hash:20060911102856-ba421-99bab57e30c6f32b64496bbbb66e8871373da0b4.gz

---
+++ lang/nl.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Dutch language file
+ *
+ * @author Riny Heijdendael <riny@heijdendael.nl>
+ */
+
+$lang['ldapab']           = 'Het LDAP adresboek';
+
+$lang['name']             = 'Achternaam';
+$lang['givenname']        = 'Voornaam';
+$lang['title']            = 'Titel';
+$lang['organization']     = 'Bedrijf';
+$lang['office']           = 'Kantoor';
+$lang['street']           = 'Straat';
+$lang['zip']              = 'Postcode';
+$lang['location']         = 'Stad';
+$lang['phone']            = 'Telefoon';
+$lang['fax']              = 'FAX';
+$lang['mobile']           = 'Mobiel';
+$lang['pager']            = 'Pager';
+$lang['homephone']        = 'Telefoon (priv.)';
+$lang['homestreet']       = 'Adres (priv.)';
+$lang['photo']            = 'Afbeelding';
+$lang['url']              = 'Website';
+$lang['note']             = 'Aantekeningen';
+$lang['mail']             = 'E-Mail';
+$lang['manager']          = 'Key Account';
+
+$lang['anniversary']      = 'Verjaardag';
+$lang['marker']           = 'Categorieen';
+$lang['tagcloud']         = 'Alle gebruikte categorieen, op alfabet gesorteerd. Veelgebruikte categorieen worden groter afgebeeld';
+
+$lang['business']         = 'Bedrijf';
+$lang['private']          = 'Prive';
+$lang['extended']         = 'Overige informatie';
+
+$lang['delphoto']         = 'Verwijder bestaande afbeelding';
+
+$lang['notloggedin']      = 'Niet ingelogd';
+$lang['loggedinas']       = 'Ingelogd als';
+$lang['login']            = 'Log in';
+$lang['logout']           = 'Log uit';
+$lang['edit']             = 'Wijzig';
+$lang['show']             = 'Toon';
+$lang['new']              = 'Nieuw';
+$lang['delete']           = 'Verwijder';
+$lang['copy']             = 'Dupliceer';
+$lang['csvexport']        = 'CSV';
+$lang['vcfexport']        = 'VCard';
+$lang['vcfimport']        = 'Importeer';
+$lang['search']           = 'Zoek';
+$lang['submit']           = 'Verstuur';
+$lang['help']             = 'Help';
+$lang['select']           = 'Selecteer';
+$lang['orgs']             = 'Bedrijven';
+$lang['upload']           = 'Upload';
+
+$lang['publicbook']       = 'Publiek Adresboek';
+$lang['privatebook']      = 'Persoonlijk Adresboek';
+
+$lang['username']         = 'Gebruikersnaam';
+$lang['password']         = 'Wachtwoord';
+$lang['remember']         = 'Blijf ingelogd op deze computer';
+
+$lang['msg_login']        = 'Log eerst in SVP!';
+$lang['msg_loginfail']    = 'Inloggen mislukt!';
+$lang['msg_reallydel']    = 'Deze definitief verwijderen?';
+$lang['msg_addto']        = 'Kies adresboek om op te slaan:';
+$lang['msg_jpegonly']     = 'Alleen JPEG afbeeldingen';
+$lang['msg_dateformat']   = 'YYYY-MM-DD';
+$lang['msg_uploadvcf']    = 'Upload VCard bestand';
+$lang['msg_tagsep']       = 'Komma gescheiden (CSV)';
+
+$lang['err_noentries']    = 'Geen gegevens';
+$lang['err_ldap']         = 'De LDAP server meldt de volgende fouten:';
+
+$lang['openxchange']      = 'Xchange Informatie';
+$lang['moreopenxchange']  = 'Xchange Informatie (meer..)';
+$lang['instantmessenger'] = 'Intstant messenger';
+$lang['categories']       = 'Categorieen';
+$lang['birthday']         = 'Verjaardag';
+$lang['domain']           = 'Email domein';
+$lang['country']          = 'Land';
+$lang['certificate']      = 'x.509 Certificaat';
+$lang['timezone']         = 'Tijdzone';
+$lang['position']         = 'Positie';
+$lang['ipphone']          = 'VOIP Telefoon';
+
+
