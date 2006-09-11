99da5ed145a17ac44abb23bfce4261c1d691242e
Geuten Edouard <thegrima@altern.org>
2006-09-11 12:31:14 +0200

    French language file
    darcs-hash:20060911103114-93f98-da4ea49ab2e98e84507a5f62b4b5d65978114481.gz

---
+++ lang/fr.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * French language file
+ *
+ * @author Geuten Edouard <thegrima@altern.org>
+ */
+
+$lang['ldapab']         = 'Le carnet d\'adresse LDAP';
+
+$lang['name']           = 'Nom de famille';
+$lang['givenname']      = 'Prénom';
+$lang['title']          = 'Titre';
+$lang['organization']   = 'Compagnie';
+$lang['office']         = 'Bureau';
+$lang['street']         = 'N° de rue';
+$lang['zip']            = 'Code Postal';
+$lang['location']       = 'Ville';
+$lang['phone']          = 'Téléphone';
+$lang['fax']            = 'FAX';
+$lang['mobile']         = 'Portable';
+$lang['pager']          = 'Pager';
+$lang['homephone']      = 'Téléphone (priv.)';
+$lang['homestreet']     = 'Adresse (priv.)';
+$lang['photo']          = 'Photo';
+$lang['url']            = 'Site web';
+$lang['note']           = 'Notes';
+$lang['mail']           = 'E-Mail';
+$lang['manager']        = 'Key Account';
+
+$lang['anniversary']    = 'Anniversaire';
+$lang['marker']         = 'Tags';
+$lang['tagcloud']       = 'Tous les marqueurs par ordre alphabétique. Les marqueurs fréquent sont montré plus grand.';
+
+$lang['business']       = 'Business';
+$lang['private']        = 'Privé';
+$lang['extended']       = 'Information Additionelle';
+
+$lang['delphoto']       = 'Effacer la photo existante';
+
+$lang['notloggedin']    = 'Non connecté';
+$lang['loggedinas']     = 'Connecté en tant que';
+$lang['login']          = 'Se connecter';
+$lang['logout']         = 'Se déconnecter';
+$lang['edit']           = 'Editer';
+$lang['show']           = 'Montrer';
+$lang['new']          = 'Nouveau';
+$lang['delete']       = 'Effacer';
+$lang['copy']         = 'Copier';
+$lang['csvexport']      = 'CSV';
+$lang['vcfexport']      = 'VCard';
+$lang['vcfimport']      = 'Import';
+$lang['search']         = 'Rechercher';
+$lang['submit']         = 'Envoyer';
+$lang['help']           = 'Aide';
+$lang['select']         = 'Sélectionner';
+$lang['orgs']           = 'Organisation';
+$lang['upload']         = 'Upload';
+
+$lang['publicbook']     = 'Carnet d\'adresse public';
+$lang['privatebook']    = 'Carnet d\'adresse privé';
+
+$lang['username']       = 'Utilisateur';
+$lang['password']       = 'Mot de passe';
+$lang['remember']       = 'Rester connecté sur cet ordinateur';
+
+$lang['msg_login']      = 'Veuillez vous connecter !';
+$lang['msg_loginfail']  = 'Echec de la connexion !';
+$lang['msg_reallydel']  = 'Voulez-vous vraiment effacer cette entrée ?';
+$lang['msg_addto']      = 'Séléectionnez le carnet d\'adresse ou ajouter une entrée:';
+$lang['msg_jpegonly']   = 'seulement JPEG';
+$lang['msg_dateformat'] = 'YYYY-MM-DD';
+$lang['msg_uploadvcf']  = 'Envoyer un fichier VCard';
+$lang['msg_tagsep']     = 'Séparé par des virgules';
+
+$lang['err_noentries']  = 'Pas d\'entrée';
+$lang['err_ldap']       = 'Le serveur LDAP à retourné les erreurs suivantes';
+
+$lang['openxchange']    = 'Xchange Information';
+$lang['moreopenxchange']    = 'Xchange Information Cont\'d';
+$lang['instantmessenger'] = 'Inst Msg';
+$lang['categories']     = 'Catégories';
+$lang['birthday']       = 'Anniversaire';
+$lang['domain']         = 'Mail Domain';
+$lang['country']        = 'Pays';
+$lang['certificate']    = 'x.509 Cert';
+$lang['timezone']       = 'Fuseau horaire';
+$lang['position']       = 'Position';
+$lang['ipphone']        = 'Téléphone IP';
+
