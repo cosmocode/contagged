b9b41f140e560c8ec28fa28c222a4a621d5c9f92
Carles Brianso <carles.brianso@antaviana.net>
2007-03-01 09:30:04 +0100

    Catalan translation
    darcs-hash:20070301083004-d8e01-21a426375bbef09f61457058314f93491f60691c.gz

---
+++ lang/ca.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Catalan language file
+ *
+ * @author Carles Briansó <carles.brianso@antaviana.net>
+ * @author Xavi Pastó <xavi.pasto@antaviana.net>
+ */
+
+$lang['ldapab']           = 'La llibreta d\'adreces LDAP';
+
+$lang['name']             = 'Cognoms';
+$lang['givenname']        = 'Nom';
+$lang['title']            = 'Títol';
+$lang['organization']     = 'Companyia';
+$lang['office']           = 'Oficina';
+$lang['street']           = 'Adreça';
+$lang['zip']              = 'Codi postal';
+$lang['location']         = 'Ciutat';
+$lang['phone']            = 'Telèfon';
+$lang['fax']              = 'Fax';
+$lang['mobile']           = 'Telefon Mòbil';
+$lang['pager']            = 'Buscapersones';
+$lang['homephone']        = 'Telèfon particular';
+$lang['homestreet']       = 'Adreça particular';
+$lang['photo']            = 'Fotografia';
+$lang['url']              = 'Pàgina web';
+$lang['note']             = 'Notes';
+$lang['mail']             = 'Correu electrònic';
+$lang['manager']          = 'Key Account';
+
+$lang['anniversary']      = 'Aniversari';
+$lang['marker']           = 'Tags';
+$lang['tagcloud']         = 'Tots els tags usats, ordenats alfabeticament. Els tags més freqüents es mostren més grans.';
+
+$lang['business']         = 'Feina';
+$lang['private']          = 'Particular';
+$lang['extended']         = 'Informació addicional';
+
+$lang['delphoto']         = 'Esborrar la foto actual';
+
+$lang['notloggedin']      = 'No esteu validats';
+$lang['loggedinas']       = 'L\'usuari validat és';
+$lang['login']            = 'Valideu-vos';
+$lang['logout']           = 'Sortir';
+$lang['edit']             = 'Editar';
+$lang['show']             = 'Veure';
+$lang['new']              = 'Nou';
+$lang['delete']           = 'Esborrar';
+$lang['copy']             = 'Copiar';
+$lang['csvexport']        = 'CSV';
+$lang['vcfexport']        = 'VCard';
+$lang['vcfimport']        = 'Importar';
+$lang['search']           = 'Buscar';
+$lang['submit']           = 'Enviar';
+$lang['help']             = 'Ajuda';
+$lang['select']           = 'Seleccionar';
+$lang['orgs']             = 'Empreses';
+$lang['upload']           = 'Pujar';
+
+$lang['publicbook']       = 'Llibreta d\'adreçes publica';
+$lang['privatebook']      = 'Llibreta d\'adreçes privada';
+
+$lang['username']         = 'Usuari';
+$lang['password']         = 'Clau';
+$lang['remember']         = 'Recorda\'m en aquest ordinador';
+
+$lang['msg_login']        = 'Valideu-vos, si us plau!';
+$lang['msg_loginfail']    = 'La validació ha fallat';
+$lang['msg_reallydel']    = 'Realment voleu esborrar aquesta l\'entrada?';
+$lang['msg_addto']        = 'Seleccioneu la llibreta d\'addreçes on afegir l\'entrada:';
+$lang['msg_jpegonly']     = 'El sistema només accepta imatges JPEG';
+$lang['msg_dateformat']   = 'YYYY-MM-DD';
+$lang['msg_uploadvcf']    = 'Pugeu un fitxer VCard';
+$lang['msg_tagsep']       = 'Separat per comes';
+
+$lang['err_noentries']    = 'No hi ha entrades';
+$lang['err_ldap']         = 'El servidor LDAP ha tornat els següents errors:';
+
+$lang['openxchange']      = 'Xchange Information';
+$lang['moreopenxchange']  = 'Xchange Information Cont\'d';
+$lang['instantmessenger'] = 'Inst Msg';
+$lang['categories']       = 'Categories';
+$lang['birthday']         = 'Aniversari';
+$lang['domain']           = 'Domini de correu';
+$lang['country']          = 'País';
+$lang['certificate']      = 'x.509 Cert';
+$lang['timezone']         = 'Zona horària';
+$lang['position']         = 'Posició';
+$lang['ipphone']          = 'Telèfon IP';
+
