36b086f43589af3a88e342bc2fc7ac1371f7efe7
Andreas Gohr <gohr@cosmocode.de>
2007-07-11 10:41:50 +0200

    Chinese language file added
    darcs-hash:20070711084150-6e07b-2b8460144f43ff0b257ac5ce1ca2243bc2432d1c.gz

---
+++ inc/lang/cn.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * Chinese language file
+ *
+ * @author George Lu <lvxueli@gmail.com>
+ */
+
+$lang['ldapab']           = 'LDAP名册';
+
+$lang['name']             = '姓';
+$lang['givenname']        =  '名';
+$lang['title']            =  '头衔';
+$lang['organization']     =  '公司';
+$lang['office']           =  '办公地点';
+$lang['street']           =  '街名号码';
+$lang['zip']              =  '邮编';
+$lang['location']         =  '城市';
+$lang['state']            =  '省';
+$lang['phone']            =  '电话';
+$lang['switchboard']      =  'Switchboard';
+$lang['fax']              =  '传真';
+$lang['mobile']           =  '手机';
+$lang['pager']            =  'BP机';
+$lang['homephone']        =  '家庭电话';
+$lang['homestreet']       =  '家庭地址';
+$lang['photo']            =  '照片';
+$lang['url']              =  '网站';
+$lang['note']             =  '注释';
+$lang['mail']             =  '电邮';
+$lang['manager']          =  '账户';
+
+$lang['anniversary']      =  '生日';
+$lang['marker']           =  '标识';
+$lang['tagcloud']         =  '所有使用过的标识,都将按照字母顺序列.常用图标将会用大字符显示.';
+
+$lang['business']         =  '公共';
+$lang['private']          =  '私人';
+$lang['extended']         =  '其它';
+$lang['communication']    =  '交流方式';
+
+$lang['delphoto']         =  '删除已有照片';
+
+$lang['notloggedin']      =  '未登录';
+$lang['loggedinas']       =  '登录为';
+$lang['login']            =  '登入';
+$lang['logout']           =  '登出';
+$lang['edit']             =  '编辑';
+$lang['show']             =  '显示';
+$lang['new']              =  '新';
+$lang['delete']           =  '删除';
+$lang['copy']             =  '拷贝';
+$lang['map']              =  '地图';
+$lang['csvexport']        =  '保存到本地CSV';
+$lang['vcfexport']        =  '保存为VCard';
+$lang['vcfimport']        =  '导出';
+$lang['search']           =  '搜索';
+$lang['submit']           =  '保存';
+$lang['help']             =  '帮助';
+$lang['select']           =  '选择';
+$lang['orgs']             =  '组织列表';
+$lang['upload']           =  '上传';
+
+$lang['publicbook']       =  '公共地址簿';
+$lang['privatebook']      =  '私有地址簿';
+
+$lang['username']         =  '用户名';
+$lang['password']         =  '密码';
+$lang['remember']         =  '保存密码';
+
+$lang['msg_login']        =  '请登录!';
+$lang['msg_loginfail']    =  '登录失败!';
+$lang['msg_reallydel']    =  '真想删除这个记录吗?';
+$lang['msg_addto']        =  '请在地址本上选择记录应该加在哪里:';
+$lang['msg_jpegonly']     =  '只能用JPEG图像';
+$lang['msg_dateformat']   =  '日期格式YYYY-MM-DD';
+$lang['msg_uploadvcf']    =  '上传VCard文件';
+$lang['msg_tagsep']       =  '用逗号分隔';
+
+$lang['err_noentries']    =  '没有记录';
+$lang['err_ldap']         =  'LDAP服务器返回下列错误';
+
+$lang['openxchange']      =  'Xchange信息';
+$lang['moreopenxchange']  =  'Xchange信息（继续）\'d';
+$lang['instantmessenger'] =  '即时信息';
+$lang['birthday']         =  '生日';
+$lang['domain']           =  '邮件域名';
+$lang['country']          =  '国家';
+$lang['certificate']      =  'x.509认证';
+$lang['timezone']         =  '时区';
+$lang['position']         =  '地点';
+$lang['department']       =  '公寓房间号';
+$lang['ipphone']          =  'IP电话';
+$lang['spouse']           =  '配偶';
