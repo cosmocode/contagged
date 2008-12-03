6893e98ad9b3e40616bde057d48a89e4145643ad
Oktay Altunergil <oktaya@gmail.com>
2008-12-03 12:53:38 +0100

    Turkish Translation
    darcs-hash:20081203115338-d5681-62b4b0be31640acb8aee695cecb6cab445d46519.gz

---
+++ inc/lang/tr.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * Turkish language file
+ *
+ * @author Oktay Altunergil <oktaya@gmail.com>
+ */
+
+$lang['ldapab']           = 'LDAP Adres Defteri';
+
+$lang['name']             = 'Soyad';
+$lang['givenname']        = 'Ad';
+$lang['title']            = 'Ünvan';
+$lang['organization']     = 'Şirket';
+$lang['office']           = 'Ofis';
+$lang['street']           = 'Adres';
+$lang['zip']              = 'Posta Kodu';
+$lang['location']         = 'Şehir';
+$lang['state']            = 'Eyalet';
+$lang['phone']            = 'Telefon';
+$lang['switchboard']      = 'Santral';
+$lang['fax']              = 'Fax';
+$lang['mobile']           = 'Cep';
+$lang['pager']            = 'Çağrı';
+$lang['homephone']        = 'Telefon (özel)';
+$lang['homestreet']       = 'Adres (özel)';
+$lang['photo']            = 'Resim';
+$lang['url']              = 'Web Sitesi';
+$lang['note']             = 'Notlar';
+$lang['mail']             = 'E-Mail';
+$lang['manager']          = 'Anahtar Hesap';
+
+$lang['anniversary']      = 'Yıldönümü';
+$lang['marker']           = 'Tagler';
+$lang['tagcloud']         = 'Kullanılan bütün tagler alfabetik olarak sıralanmıştır. Sıkça kullanılan tagler daha büyük gösterilmiştir.';
+
+$lang['business']         = 'İş';
+$lang['private']          = 'Özel';
+$lang['extended']         = 'İlave Bilgiler';
+$lang['communication']    = 'Haberleşme';
+
+$lang['delphoto']         = 'Var olan resmi sil';
+
+$lang['notloggedin']      = 'Giriş yapılmamış';
+$lang['loggedinas']       = 'Giriş yapmiş kullanıcı';
+$lang['login']            = 'Giriş Yap';
+$lang['logout']           = 'Sistemten Çık';
+$lang['edit']             = 'Düzenle';
+$lang['show']             = 'Göster';
+$lang['new']              = 'Yeni';
+$lang['delete']           = 'Sil';
+$lang['copy']             = 'Kopyala';
+$lang['map']              = 'Harita';
+$lang['csvexport']        = 'CSV';
+$lang['vcfexport']        = 'VCard';
+$lang['vcfimport']        = 'İçe getir';
+$lang['search']           = 'Ara';
+$lang['submit']           = 'Kaydet';
+$lang['help']             = 'Yardım';
+$lang['select']           = 'Seç';
+$lang['orgs']             = 'Şirketler';
+$lang['upload']           = 'Yükle';
+
+$lang['publicbook']       = 'Açık Adres Defteri';
+$lang['privatebook']      = 'Özel Adres Defteri';
+
+$lang['username']         = 'Kullanıcı Adı';
+$lang['password']         = 'Parola';
+$lang['remember']         = 'Bu bilgisayarda bağlı kal';
+
+$lang['msg_login']        = 'Lütfen giriş yapınız!';
+$lang['msg_loginfail']    = 'Giriş başarısız!';
+$lang['msg_reallydel']    = 'Gerçekten silinsin mi?';
+$lang['msg_addto']        = 'Ekleme yapmak istediğiniz adres defterini seçiniz:';
+$lang['msg_jpegonly']     = 'Sadece JPEG';
+$lang['msg_dateformat']   = 'YYYY-MM-DD';
+$lang['msg_uploadvcf']    = 'VCard dosyası yükle';
+$lang['msg_tagsep']       = 'Virgülle ayrılmış';
+
+$lang['err_noentries']    = 'Sonuç yok';
+$lang['err_ldap']         = 'LDAP sunucusundan aşağıdaki hata gönderildi.';
+
+$lang['openxchange']      = 'Xchange bilgisi';
+$lang['moreopenxchange']  = 'Xchange bilgisi (devam)';
+$lang['instantmessenger'] = 'Anında İletişim';
+$lang['birthday']         = 'Doğum günü';
+$lang['domain']           = 'E-Posta Domain';
+$lang['country']          = 'Ülke';
+$lang['certificate']      = 'x.509 Cert';
+$lang['timezone']         = 'Zaman Dilimi';
+$lang['position']         = 'Pozisyon';
+$lang['department']       = 'Bölüm';
+$lang['ipphone']          = 'IP Telefon';
+$lang['spouse']           = 'Eşi';
