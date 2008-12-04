d2c8cbcc25cd28ebd4ebbe278901d0863fe2a56f
Stanislav <ginermail@gmail.com>
2008-12-04 15:10:21 +0100

    Automatically create a private address book on login
    darcs-hash:20081204141021-e4d75-de37a76b7883c6935146c611ff794f5d679eeb66.gz

--- login.php
+++ login.php
@@ -6,9 +6,20 @@
 require_once('inc/init.php');
 
 $msg = $lang['msg_login'];
-if(isset($_REQUEST['username'])){
+if(!empty($_REQUEST['username'])){
     if (empty($_REQUEST['password'])) { $_REQUEST['password']=''; }
     if (do_ldap_bind($_REQUEST['username'],$_REQUEST['password'])){
+
+        //create private address book if simple enough
+        if(preg_match('/ou=([^,]+)$/',$conf['privatebook'],$match)){
+            $privatedn = $conf['privatebook'].', '.$_SESSION['ldapab']['binddn'];
+            if(!ldap_read($LDAP_CON,$privatedn,'')){
+                ldap_add($LDAP_CON,$privatedn,
+                         array('objectClass' => array ('organizationalUnit','top'),
+                               'ou' => $match[1]));
+            }
+        }
+
         //forward to next page
         if(!empty($_SESSION['ldapab']['lastlocation'])){
             header('Location: '.$_SESSION['ldapab']['lastlocation']);
