1e6dfc6287490948926b3aff8324a451502a3524
Stanislav <ginermail@gmail.com>
2008-12-11 16:13:55 +0100

    Russian translation
    darcs-hash:20081211151355-e4d75-9c358fdd009e6d2096ab3646e8e29970cb2f17bb.gz

--- inc/lang/ru.php
+++ inc/lang/ru.php
@@ -1,3 +1,98 @@
 <?php
+/**
+ * Russian language file
+ *
+ * @author Stanislav GE <ginermail@gmail.com>
+ */
+
+$lang['ldapab']           = 'Адресная книга LDAP';
+
+$lang['name']             = 'Фамилия';
+$lang['givenname']        = 'Имя';
+$lang['title']            = 'Должность';
+$lang['organization']     = 'Организация';
+$lang['office']           = 'Офис';
+$lang['street']           = 'Улица';
+$lang['zip']              = 'Почтовый индекс';
+$lang['location']         = 'Город';
+$lang['state']            = 'Штат';
+$lang['phone']            = 'Телефон';
+$lang['switchboard']      = 'Switchboard';
+$lang['fax']              = 'Факс';
+$lang['mobile']           = 'Мобильный телефон';
+$lang['pager']            = 'Пейджер';
+$lang['homephone']        = 'Домашний телефон';
+$lang['homestreet']       = 'Домашний адрес';
+$lang['photo']            = 'Фото';
+$lang['url']              = 'Веб-сайт';
+$lang['note']             = 'Комментарий';
+$lang['mail']             = 'Электронный адрес (E-Mail)';
+$lang['manager']          = 'Аккаунт-менеджер';
+
+$lang['anniversary']      = 'Дата рождения';
+$lang['marker']           = 'Метки';
+$lang['tagcloud']         = 'Метки сортируются по алфавиту. Наиболее часто используемые метки имеют больший размер.';
+
+$lang['business']         = 'Работа';
+$lang['private']          = 'Личное';
+$lang['extended']         = 'Дополнительная информация';
+$lang['communication']    = 'Связь';
+
+$lang['delphoto']         = 'Удалить фото';
+
+$lang['notloggedin']      = 'Не вошли';
+$lang['loggedinas']       = 'Вошли как';
+$lang['login']            = 'Вход';
+$lang['logout']           = 'Выход';
+$lang['edit']             = 'Редактировать';
+$lang['show']             = 'Посмотреть';
+$lang['new']              = 'Новый';
+$lang['delete']           = 'Удалить';
+$lang['copy']             = 'Копировать';
+$lang['map']              = 'Карта';
+$lang['csvexport']        = 'CSV';
+$lang['vcfexport']        = 'VCard';
+$lang['vcfimport']        = 'Импорт';
+$lang['search']           = 'Найти';
+$lang['submit']           = 'Сохранить';
+$lang['help']             = 'Помощь';
+$lang['select']           = 'Выбрать';
+$lang['orgs']             = 'Компании';
+$lang['upload']           = 'Загрузить';
+
+$lang['publicbook']       = 'Общая адресная книга';
+$lang['privatebook']      = 'Личная адресная книга';
+
+$lang['username']         = 'Имя пользователя';
+$lang['password']         = 'Пароль';
+$lang['remember']         = 'Запомнить на этом компьютере';
+
+$lang['msg_login']        = 'Необходимо войти!';
+$lang['msg_loginfail']    = 'Ошибка входа!';
+$lang['msg_reallydel']    = 'Точно удалить?';
+$lang['msg_addto']        = 'Выберете адресную книгу в которую хотите добавить запись:';
+$lang['msg_jpegonly']     = 'Только JPEG';
+$lang['msg_dateformat']   = 'ГГГГ-ММ-ДД';
+$lang['msg_uploadvcf']    = 'Скачать VCard';
+$lang['msg_tagsep']       = 'Текст с разделителями';
+
+$lang['err_noentries']       = 'Нет записей';
+$lang['err_ldap']            = 'Сервер LDAP возвратил ошибки';
+$lang['err_wrongFileType']   = 'Файл не является изображением.';
+$lang['err_fileNotUploaded'] = 'Файл не может быть загружен, может быть слишком велик?';
+
+$lang['openxchange']      = 'Xchange Information';
+$lang['moreopenxchange']  = 'Xchange Information Cont\'d';
+$lang['instantmessenger'] = 'Служба мгновенных сообщений';
+$lang['birthday']         = 'День рождения';
+$lang['domain']           = 'Почтовый домен';
+$lang['country']          = 'Страна';
+$lang['certificate']      = 'x.509 сертификат';
+$lang['timezone']         = 'Часовой пояс';
+$lang['position']         = 'Должность';
+$lang['department']       = 'Подразделение';
+$lang['ipphone']          = 'IP Телефон';
+$lang['spouse']           = 'Семейное положение';
 
 $lang['lettertabs'] = 'А Б В Г Д Е Ё Ж З И Й К Л М Н О П Р С Т У Ф Х Ц Ч Ш Щ Ъ Ы Ь Э Ю Я';
+
