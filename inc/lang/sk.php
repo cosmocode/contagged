88c7097e6f942052991d5e6a190896f8ae1aacee
Peter Tuhársky <tuharsky@misbb.sk>
2009-10-20 10:50:27 +0200

    Slovak Translation added

---
+++ inc/lang/sk.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * Slovak language file
+ *
+ * @author Peter Tuhársky <tuharsky@misbb.sk>
+ */
+
+$lang['ldapab']           = 'LDAP adresár';
+
+$lang['name']             = 'Priezvisko';
+$lang['givenname']        = 'Krstné meno';
+$lang['title']            = 'Titul';
+$lang['organization']     = 'Organizácia';
+$lang['office']           = 'Kancelária';
+$lang['street']           = 'Ulica číslo';
+$lang['zip']              = 'PSČ';
+$lang['location']         = 'Mesto';
+$lang['state']            = 'Štát';
+$lang['phone']            = 'Telefón';
+$lang['switchboard']      = 'Switchboard';
+$lang['fax']              = 'FAX';
+$lang['mobile']           = 'Mobil';
+$lang['pager']            = 'Pager';
+$lang['homephone']        = 'Telefón domov';
+$lang['homestreet']       = 'Adresa domov';
+$lang['photo']            = 'Fotka';
+$lang['url']              = 'Webová stránka';
+$lang['note']             = 'Poznámky';
+$lang['mail']             = 'E-Mail';
+$lang['manager']          = 'Key Account';
+
+$lang['anniversary']      = 'Výročie';
+$lang['marker']           = 'Značky';
+$lang['tagcloud']         = 'Všetky používané značky, abecedne zoradené. Často používané značky sú zobrazené väčšie.';
+
+$lang['business']         = 'Obchodné';
+$lang['private']          = 'Súkromné';
+$lang['extended']         = 'Doplňujúce informácie';
+$lang['communication']    = 'Komunikácia';
+
+$lang['delphoto']         = 'Odstrániť existujúcu fotku';
+
+$lang['notloggedin']      = 'Neprihlásený';
+$lang['loggedinas']       = 'Prihlásený ako';
+$lang['login']            = 'Prihlásiť';
+$lang['logout']           = 'Odhlásiť';
+$lang['edit']             = 'Upraviť';
+$lang['show']             = 'Zobraziť';
+$lang['new']              = 'Nové';
+$lang['delete']           = 'Odstrániť';
+$lang['copy']             = 'Kópia';
+$lang['map']              = 'Mapa';
+$lang['csvexport']        = 'CSV';
+$lang['vcfexport']        = 'VCard';
+$lang['vcfimport']        = 'Import';
+$lang['search']           = 'Hľadať';
+$lang['submit']           = 'Odoslať';
+$lang['help']             = 'Pomoc';
+$lang['select']           = 'Vybrať';
+$lang['orgs']             = 'Organizácie';
+$lang['upload']           = 'Odovzdať';
+$lang['qrcode']           = 'QR kód';
+
+$lang['publicbook']       = 'Verejný adresár';
+$lang['privatebook']      = 'Súkromný adresár';
+
+$lang['username']         = 'Používateľské meno';
+$lang['password']         = 'Heslo';
+$lang['remember']         = 'Ostať prihlásený z tohto počítača';
+
+$lang['msg_login']        = 'Prosím prihláste sa!';
+$lang['msg_loginfail']    = 'Prihlásenie zlyhalo!';
+$lang['msg_reallydel']    = 'Skutočne chcete odstrániť túto položku?';
+$lang['msg_addto']        = 'Vyberte adresár, do ktorého sa má pridať táto položka:';
+$lang['msg_jpegonly']     = 'Iba JPEG ';
+$lang['msg_dateformat']   = 'RRRR-MM-DD';
+$lang['msg_uploadvcf']    = 'Odovzdať súbor VCard';
+$lang['msg_tagsep']       = 'Oddelené čiarkou';
+
+$lang['err_noentries']       = 'Žiadne položky';
+$lang['err_ldap']            = 'LDAP server vrátil nasledovné chyby';
+$lang['err_wrongFileType']   = 'Tento súbor nie je obrázkom.';
+$lang['err_fileNotUploaded'] = 'Tento súbor sa nedá odovzdať, možno je príliš veľký?';
+
+$lang['openxchange']      = 'Informácie o Xchange';
+$lang['moreopenxchange']  = 'Xchange Information Cont\'d';
+$lang['instantmessenger'] = 'Inst Msg';
+$lang['birthday']         = 'Narodeniny';
+$lang['domain']           = 'Poštová doména';
+$lang['country']          = 'Krajina';
+$lang['certificate']      = 'x.509 Cert';
+$lang['timezone']         = 'Časové pásmo';
+$lang['position']         = 'Pozícia';
+$lang['department']       = 'Oddelenie';
+$lang['ipphone']          = 'IP Telefón';
+$lang['spouse']           = 'Spouse';
+
+$lang['lettertabs'] = 'A Á B C Č D Ď DZ DŽ E É F G H CH I Í J K L Ľ M N Ň O Ó P R S Š T Ť U Ú V W X Y Z Ž';
