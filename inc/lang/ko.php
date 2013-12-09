e5d1ac3c1bffa7bc405658484abc463e2955f807
Myeongjin <aranet100@gmail.com>
2013-12-09 13:47:25 +0900

    Add Korean translation

---
+++ inc/lang/ko.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * Korean language file
+ *
+ * @author Myeongjin <aranet100@gmail.com>
+ */
+
+$lang['ldapab']           = 'LDAP 주소록';
+
+$lang['name']             = '성';
+$lang['givenname']        = '이름';
+$lang['title']            = '제목';
+$lang['organization']     = '회사';
+$lang['office']           = '사무실';
+$lang['street']           = '거리 번호';
+$lang['zip']              = '우편 번호';
+$lang['location']         = '시';
+$lang['state']            = '주';
+$lang['phone']            = '전화';
+$lang['switchboard']      = '배전반';
+$lang['fax']              = '팩스';
+$lang['mobile']           = '휴대 전화';
+$lang['pager']            = '호출기';
+$lang['homephone']        = '전화 (비공개)';
+$lang['homestreet']       = '주소 (비공개)';
+$lang['photo']            = '사진';
+$lang['url']              = '웹사이트';
+$lang['note']             = '참고';
+$lang['mail']             = '이메일';
+$lang['manager']          = '키 계정';
+
+$lang['anniversary']      = '기념일';
+$lang['marker']           = '태그';
+$lang['tagcloud']         = '모든 사용된 태그는, 알파벳순으로 정렬됩니다. 자주 사용된 태그는 크게 보여집니다.';
+
+$lang['business']         = '업무';
+$lang['private']          = '비공개';
+$lang['extended']         = '추가적인 정보';
+$lang['communication']    = '의사 소통';
+
+$lang['delphoto']         = '기존 사진 삭제';
+
+$lang['notloggedin']      = '로그인하지 않음';
+$lang['loggedinas']       = '로그인한 사용자';
+$lang['login']            = '로그인';
+$lang['logout']           = '로그아웃';
+$lang['edit']             = '편집';
+$lang['show']             = '보기';
+$lang['new']              = '새로';
+$lang['delete']           = '삭제';
+$lang['copy']             = '복사';
+$lang['map']              = '지도';
+$lang['csvexport']        = 'CSV';
+$lang['vcfexport']        = 'VCard';
+$lang['vcfimport']        = '가져오기';
+$lang['search']           = '검색';
+$lang['submit']           = '저장';
+$lang['help']             = '도움말';
+$lang['select']           = '선택';
+$lang['orgs']             = '회사';
+$lang['upload']           = '올리기';
+$lang['qrcode']           = 'QR 코드';
+
+$lang['publicbook']       = '공개 주소록';
+$lang['privatebook']      = '비공개 주소록';
+
+$lang['username']         = '사용자 이름';
+$lang['password']         = '비밀번호';
+$lang['remember']         = '이 컴퓨터에서 로그인 유지';
+
+$lang['msg_login']        = '로그인하세요!';
+$lang['msg_loginfail']    = '로그인에 실패했습니다!';
+$lang['msg_reallydel']    = '항목을 삭제하겠습니까?';
+$lang['msg_addto']        = '항목을 추가해야할 주소롤을 선택하세요:';
+$lang['msg_jpegonly']     = 'JPEG만';
+$lang['msg_dateformat']   = 'YYYY-MM-DD';
+$lang['msg_uploadvcf']    = 'VCard 파일 올리기';
+$lang['msg_tagsep']       = '쉼표로 구분';
+
+$lang['err_noentries']       = '항목 없음';
+$lang['err_ldap']            = 'LDAP 서버가 다음 오류를 반환했습니다';
+$lang['err_wrongFileType']   = '파일은 그림이 아닙니다.';
+$lang['err_fileNotUploaded'] = '파일을 올릴 수 없습니다, 막대했을 수도 있습니다?';
+
+$lang['openxchange']      = 'Xchange 정보';
+$lang['moreopenxchange']  = 'Xchange 정보 계속\'d';
+$lang['instantmessenger'] = '인스턴트 메신저';
+$lang['birthday']         = '생일';
+$lang['domain']           = '메일 도메인';
+$lang['country']          = '나라';
+$lang['certificate']      = 'x.509 인증서';
+$lang['timezone']         = '시간대';
+$lang['position']         = '위치';
+$lang['department']       = '부분';
+$lang['ipphone']          = 'IP 전화';
+$lang['spouse']           = '배우자';
+
+$lang['lettertabs'] = 'ㄱ ㄴ ㄷ ㄹ ㅁ ㅂ ㅅ ㅇ ㅈ ㅊ ㅋ ㅌ ㅍ ㅎ A B C D E F G H I J K L M N O P Q R S T U V W X Y Z';
